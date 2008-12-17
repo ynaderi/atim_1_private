abcbcd4be6d64cf529e35ca0e773b6da7226c41c
aaronsuggitt <aaronsuggitt@9a8715c8-bb36-4f25-a4f9-89e91ce7aac0>
2008-12-17 22:15:36 +0000

    Added changelog.txt and .sql upgrade for next minor release.

---
+++ docs/upgrades/atim/v1.5.1/v1.5.1_upgrade.sql
@@ -0,0 +1,31 @@
+-- ----------------------------------------------------------------------------
+-- CLINICAL ANNOTATION
+-- ----------------------------------------------------------------------------
+
+/* New lookup 'Pager' to ParticipantContact.phone_type and ParticipantContact.phone2_type */
+
+INSERT INTO `global_lookups` (`id`, `alias`, `section`, `subsection`, `value`, `language_choice`, `display_order`, `active`, `created`, `created_by`, `modified`, `modified_by`) VALUES
+(NULL, 'phone_type', NULL, NULL, 'pager', 'pager', '6', 'yes', NULL, NULL, NULL, NULL);
+
+INSERT INTO `form_fields_global_lookups` ( `field_id` , `lookup_id` ) VALUES
+('CAN-999-999-000-999-49', (SELECT `id` FROM `global_lookups` WHERE `alias` = 'phone_type' AND `value` = 'pager') ),
+('CAN-999-999-000-999-50', (SELECT `id` FROM `global_lookups` WHERE `alias` = 'phone_type' AND `value` = 'pager') );
+
+INSERT INTO `i18n` ( `id` , `page_id` , `en` , `fr` ) VALUES
+('pager', 'global', 'Pager', 'Paget');
+
+SELECT `id` FROM `global_lookups` WHERE `alias` = 'phone_type' AND `value` = 'pager'
+
+
+/* Drop Brachytherapy related treatment tables  */
+
+DROP TABLE `txe_brachytherapies`;
+DROP TABLE `txd_brachytherapies`;
+
+
+/* Drop depricated fields from Lifestyle base form  */
+
+ALTER TABLE `ed_all_lifestyle_base`
+  DROP `prior_cancer_dx`,
+  DROP `prior_cancer_dx_year`,
+  DROP `prior_cancer_tx`;
