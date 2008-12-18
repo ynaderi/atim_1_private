4e1b4f55d227401a882ba80e2a5d26fb82e029e0
nluc <nluc@9a8715c8-bb36-4f25-a4f9-89e91ce7aac0>
2008-12-18 15:31:15 +0000

    Add delete statement on form_fields_global_lookup

--- docs/upgrades/atim/v1.5.1/v1.5.1_upgrade.sql
+++ docs/upgrades/atim/v1.5.1/v1.5.1_upgrade.sql
@@ -11,6 +11,12 @@ AND `value` = 'pager';
 INSERT INTO `global_lookups` (`id`, `alias`, `section`, `subsection`, `value`, `language_choice`, `display_order`, `active`, `created`, `created_by`, `modified`, `modified_by`) VALUES
 (NULL, 'phone_type', NULL, NULL, 'pager', 'pager', '6', 'yes', NULL, NULL, NULL, NULL);
 
+DELETE FROM `form_fields_global_lookups` WHERE `field_id` = 'CAN-999-999-000-999-49'
+AND `lookup_id` IN (SELECT `id` FROM `global_lookups` WHERE `alias` = 'phone_type' AND `value` = 'pager');
+
+DELETE FROM `form_fields_global_lookups` WHERE `field_id` = 'CAN-999-999-000-999-50'
+AND `lookup_id` IN (SELECT `id` FROM `global_lookups` WHERE `alias` = 'phone_type' AND `value` = 'pager');
+
 INSERT INTO `form_fields_global_lookups` ( `field_id` , `lookup_id` ) VALUES
 ('CAN-999-999-000-999-49', (SELECT `id` FROM `global_lookups` WHERE `alias` = 'phone_type' AND `value` = 'pager') ),
 ('CAN-999-999-000-999-50', (SELECT `id` FROM `global_lookups` WHERE `alias` = 'phone_type' AND `value` = 'pager') );
