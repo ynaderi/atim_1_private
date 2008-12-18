913f855f82c5798236a21c23022ad587f68a5d65
nluc <nluc@9a8715c8-bb36-4f25-a4f9-89e91ce7aac0>
2008-12-18 15:28:32 +0000

    Add IF EXISTS. Add DELETE statment. Add a dot coma,

--- docs/upgrades/atim/v1.5.1/v1.5.1_upgrade.sql
+++ docs/upgrades/atim/v1.5.1/v1.5.1_upgrade.sql
@@ -4,6 +4,10 @@
 
 /* New lookup 'Pager' to ParticipantContact.phone_type and ParticipantContact.phone2_type */
 
+DELETE FROM `global_lookups`
+WHERE `alias` = 'phone_type'
+AND `value` = 'pager';
+
 INSERT INTO `global_lookups` (`id`, `alias`, `section`, `subsection`, `value`, `language_choice`, `display_order`, `active`, `created`, `created_by`, `modified`, `modified_by`) VALUES
 (NULL, 'phone_type', NULL, NULL, 'pager', 'pager', '6', 'yes', NULL, NULL, NULL, NULL);
 
@@ -11,16 +15,19 @@ INSERT INTO `form_fields_global_lookups` ( `field_id` , `lookup_id` ) VALUES
 ('CAN-999-999-000-999-49', (SELECT `id` FROM `global_lookups` WHERE `alias` = 'phone_type' AND `value` = 'pager') ),
 ('CAN-999-999-000-999-50', (SELECT `id` FROM `global_lookups` WHERE `alias` = 'phone_type' AND `value` = 'pager') );
 
+DELETE FROM `i18n`
+WHERE `id` = 'pager';
+
 INSERT INTO `i18n` ( `id` , `page_id` , `en` , `fr` ) VALUES
 ('pager', 'global', 'Pager', 'Paget');
 
-SELECT `id` FROM `global_lookups` WHERE `alias` = 'phone_type' AND `value` = 'pager'
+SELECT `id` FROM `global_lookups` WHERE `alias` = 'phone_type' AND `value` = 'pager';
 
 
 /* Drop Brachytherapy related treatment tables  */
 
-DROP TABLE `txe_brachytherapies`;
-DROP TABLE `txd_brachytherapies`;
+DROP TABLE  IF EXISTS `txe_brachytherapies`;
+DROP TABLE  IF EXISTS `txd_brachytherapies`;
 
 
 /* Drop depricated fields from Lifestyle base form  */
